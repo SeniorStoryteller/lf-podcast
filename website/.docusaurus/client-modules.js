cf064633cd458099e03d901495f9865203a368d7
James McLeod <james.mcleod@finos.org>
2022-04-05 13:17:53 +0100

    delete docusaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/jamesmcleod/Projects/lf-podcast/lf-podcast/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jamesmcleod/Projects/lf-podcast/lf-podcast/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jamesmcleod/Projects/lf-podcast/lf-podcast/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/jamesmcleod/Projects/lf-podcast/lf-podcast/website/src/css/custom.css"),
-];
