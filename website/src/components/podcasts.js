c82efb564bc551e23adda3fd8c63f27b51e1c838
James McLeod <james.mcleod@finos.org>
2022-04-19 10:04:42 +0100

    add podcast templates and config

---
+++ website/src/components/podcasts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const podcasts = { 
+    "podcast1" : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1244739151&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+    "podcast2" : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1237986250&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+    "podcast3" : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1231124572&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+};
