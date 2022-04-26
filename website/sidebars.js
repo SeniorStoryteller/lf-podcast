4bebcfb32cc919ced169447d4db62202b9ecdc84
James McLeod <james.mcleod@finos.org>
2022-04-26 17:32:51 +0100

    add safe podcast

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   mainSidebar: {
     "Linux Foundation": ["podcasts-intro", "podcasts/podcast-index"],
-    "Podcasts" : ["podcasts/fast-times-at-sbom-high"],
+    "Podcasts" : ["podcasts/fast-times-at-sbom-high", "podcasts/safe-or-unsafe-at-any-speed"],
     "Adding New Podcasts": ["templates/podcast-template"]
   },
 }
