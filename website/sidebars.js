3b3fbb58eaa7771a30152ae135be856c60593a27
James McLeod <james.mcleod@finos.org>
2022-04-26 17:22:54 +0100

    add sbom

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   mainSidebar: {
     "Linux Foundation": ["podcasts-intro", "podcasts/podcast-index"],
+    "Podcasts" : ["podcasts/fast-times-at-sbom-high"],
     "Adding New Podcasts": ["templates/podcast-template"]
   },
 }
